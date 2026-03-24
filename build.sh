333598644c87b2d7348314b31c10952e4a037e25
Rahul <114048450+Rahul8720@users.noreply.github.com>
2026-03-24 12:10:18 +0530

    Update Docker build and run commands in build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-#login into DockerHub:
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
 
-#stopping existing container:
-docker stop react
-docker rm react
+docker stop deploy-project
+docker rm deploy-project
 
-#building a image:
-docker build -t react-ci/cd .
 
-#running a container from the created image:
-docker run -d -it --name react -p 80:80 react-ci/cd
+docker build -t react-project .
 
-#pushing the image to dockerhub:
-docker tag react-ci/cd naveen712/react-app:ci-cd
-docker push naveen712/react-app:ci-cd
+docker run -itd --name deploy-project -p 555:80 react-project
+
+docker tag react-project rahul9786/react-project-1
+
+docker push rahul9786/react-project-1
