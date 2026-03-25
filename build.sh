8b20d8ce88ca098e1406f0ec81cff4fa4c811268
root <root@ip-172-31-26-175.ec2.internal>
2026-03-25 10:03:09 +0000

    fixed build script

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker stop deploy-project
-docker rm deploy-project
-
+docker stop deploy-project || true
+docker rm deploy-project || true
 
 docker build -t react-project .
 
-docker run -itd --name deploy-project -p 555:80 react-project
+docker run -d --name deploy-project -p 555:80 react-project
 
-docker tag react-project rahul9786/react-project-1
+docker tag react-project rahul9786/react-project-1:latest
 
-docker push rahul9786/react-project-1
+docker push rahul9786/react-project-1:latest
